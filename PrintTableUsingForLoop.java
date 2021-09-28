2d4fed732305d891c68dd030db65e1f16af56ee9
kirankumaretoori <kirankumar.etoori@gmail.com>
2021-09-28 23:50:38 +0530

    Lab 5
    Lab 5 of Java

---
+++ PrintTableUsingForLoop.java
@@ -0,0 +1,16 @@
+package lab5;
+
+//1. Write a number to print the table of 5, first 10 values
+
+public class PrintTableUsingForLoop {
+
+	public static void main(String[] args) {
+
+		System.out.println("######### Printing 5 table #########");
+
+		for (int i = 1; i <= 10; i++)
+			System.out.println(5 + " * " + i + " = " + (5 * i));
+
+	}
+
+}
